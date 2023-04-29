f26e60c6d768fe486a1516ba860703696a69d4d1
Cornelius2017 <xforealn@gmail.com>
2023-04-29 10:05:47 +0300

    seo && fav

---
+++ src/components/Seo/index.jsx
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+
+export default function Seo({ seo }) {
+    return (
+        <Head>
+            {/* Fav base (needed advanced) */}
+            <link href="fav/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+            <link href="fav/favicon.ico" rel="icon" type="image/x-icon" />
+
+            <link rel="shortcut icon" href="fav/favicon.png" type="image/x-icon" />
+            <link rel="icon" href="fav/favicon.png" type="image/x-icon" />
+
+            <link rel="apple-touch-icon" sizes="144x144" href="fav/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="fav/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="fav/favicon-16x16.png" />
+            <link rel="mask-icon" href="fav/safari-pinned-tab.svg" color="#fff" />
+
+            <link rel="manifest" href="fav/site.webmanifest" />
+            <link rel="mask-icon" href="fav/safari-pinned-tab.svg" color="#3a1b70" />
+            <meta name="msapplication-TileColor" content="#3a1b70" />
+            <meta name="theme-color" content="#3a1b70" />
+
+            {/* Canonical link tag */}
+            <link rel="canonical" href={seo.metaURL} />
+
+            {/* Main SEO meta */}
+            <title key="title">{seo.metaHeading}</title>
+            <meta name="description" content={seo.metaDescription} key="description" />
+            <meta name="author" content="Epicurus && Anastasiia_Berest"></meta>
+
+            {/* Schema.org markup for Google+ */}
+            <meta itemProp="name" content={seo.metaHeading} key="itemPropname" />
+            <meta itemProp="description" content={seo.metaDescription} key="itemPropdescription" />
+            <meta itemProp="image" content={seo.metaImg} key="itemPropimage" />
+
+            {/* Twitter Card data */}
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content={seo.metaURL} key="twitter:site" />
+            <meta name="twitter:title" content={seo.metaHeading} key="twitter:title" />
+            <meta name="twitter:description" content={seo.metaDescription} key="twitter:description" />
+            <meta name="twitter:creator" content="MixFin" key="twitter:creator" />
+            <meta name="twitter:image" content={seo.metaImg} key="twitter:image" />
+
+            {/* Open Graph data  */}
+            <meta property="og:url" key="og:url" content={seo.metaURL} />
+            <meta property="og:site_name" key="og:site_name" content={seo.metaSiteName} />
+            <meta property="og:title" key="og:title" content={seo.metaHeading} />
+            <meta property="og:description" key="og:description" content={seo.metaDescription} />
+            <meta property="og:type" key="og:type" content="website" />
+            <meta property="og:locale" key="og:locale" content={seo.metaLocale} />
+            <meta property="og:image" key="og:image" content={seo.metaImg} />
+            <meta property="og:image:width" key="og:image:width" content={seo.metaImgWidth} />
+            <meta property="og:image:height" key="og:image:height" content={seo.metaImgHeight} />
+
+        </Head>
+    )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,64 @@
-import Head from 'next/head'
-
+import Seo from '../components/Seo'
 import '../assets/styles/main.sass'
 
+const seo = {
+    metaHeading: 'Epicurus',
+    metaDescription: 'Epicurus Description',
+    metaSiteName: 'Epicurus',
+    metaLocale: 'en',
+    metaURL: `${process.env.NEXT_PUBLIC_HOST}`,
+    //metaImg: `${process.env.NEXT_PUBLIC_HOST}/og.jpg`,
+    metaImg: 'og.jpg',
+    metaImgWidth: '600',
+    metaImgHeight: '314',
+}
+
+const schema = {
+    '@context': 'http://schema.org/',
+    '@type': 'Organization',
+    name: 'Epicurus',
+    brand: 'Epicurus',
+    alternateName: 'Epicurus',
+    url: process.env.NEXT_PUBLIC_HOST,
+    logo: `${process.env.NEXT_PUBLIC_HOST}/ua/logo.svg`,
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'SOHO The Strand, Fawwara Building, Triq L-Imsida',
+      addressLocality: 'Gzira',
+      postalCode: 'GZR 1362',
+      addressCountry: 'Malta',
+    },
+    contactPoint: [
+      {
+        '@type': 'ContactPoint',
+        telephone: '380505635807',
+        email: 'info@epicurus.com',
+        contactType: 'customer support',
+        areaServed: 'US',
+        availableLanguage: ['English'],
+      },
+    ],
+    sameAs: [
+      'https://www.facebook.com/epicurus',
+      'https://twitter.com/epicurus',
+      'https://www.linkedin.com/company/epicurus',
+      'https://www.instagram.com/epicurus',
+    ]
+  }
+  
 
 export default function App({ Component, pageProps }) {
     return (
         <>
-            <Head> Header </Head>
-            <Component {...pageProps} />
+            <Seo seo={seo}/>
+            <script type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(schema)
+            }}></script>
+
+            <header>header</header>
+            <Component {...pageProps}/>
+            <footer>footer</footer>
         </>
     )
 }
